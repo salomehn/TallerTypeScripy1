53bda510f6db875d3a78192cd6ef64e6404ff268
salomehn <m.hernandezn@uniandes.edu.co>
2026-04-13 01:13:54 -0500

    Taller 2

--- main.ts
+++ main.ts
@@ -1,15 +1,27 @@
 import { series } from "./data.js";
+import { Serie } from "./serie.js";
 
 const tableBody: HTMLElement = document.getElementById("series-table-body")!;
 const averageElement: HTMLElement = document.getElementById("average")!;
+const detailElement: HTMLElement = document.getElementById("series-detail")!;
 
 function renderSeries(): void {
   series.forEach((serie) => {
     let row = document.createElement("tr");
-    row.innerHTML = `<td>${serie.id}</td>
-      <td><a href="${serie.webpage}" target="_blank">${serie.name}</a></td>
+
+    row.innerHTML = `
+      <td>${serie.id}</td>
+      <td><a href="#" class="serie-link">${serie.name}</a></td>
       <td>${serie.channel}</td>
-      <td>${serie.seasons}</td>`;
+      <td>${serie.seasons}</td>
+    `;
+
+    const link = row.querySelector(".serie-link") as HTMLAnchorElement;
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      showDetail(serie);
+    });
+
     tableBody.appendChild(row);
   });
 }
@@ -28,5 +40,23 @@ function mostrarPromedio(): void {
   averageElement.innerHTML = `Seasons average: ${calcularPromedio()}`;
 }
 
+function showDetail(serie: Serie): void {
+  detailElement.innerHTML = `
+    <div class="card">
+      <img class="card-img-top" src="${serie.image}" alt="${serie.name}">
+      <div class="card-body">
+        <h5 class="card-title">${serie.name}</h5>
+        <p class="card-text">${serie.description}</p>
+        <a href="${serie.webpage}" target="_blank">${serie.webpage}</a>
+      </div>
+    </div>
+  `;
+}
+
 renderSeries();
 mostrarPromedio();
+
+const firstSerie = series[0];
+if (firstSerie) {
+  showDetail(firstSerie);
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,22 @@
 import { series } from "./data.js";
+import { Serie } from "./serie.js";
 const tableBody = document.getElementById("series-table-body");
 const averageElement = document.getElementById("average");
+const detailElement = document.getElementById("series-detail");
 function renderSeries() {
     series.forEach((serie) => {
         let row = document.createElement("tr");
-        row.innerHTML = `<td>${serie.id}</td>
-      <td><a href="${serie.webpage}" target="_blank">${serie.name}</a></td>
+        row.innerHTML = `
+      <td>${serie.id}</td>
+      <td><a href="#" class="serie-link">${serie.name}</a></td>
       <td>${serie.channel}</td>
-      <td>${serie.seasons}</td>`;
+      <td>${serie.seasons}</td>
+    `;
+        const link = row.querySelector(".serie-link");
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            showDetail(serie);
+        });
         tableBody.appendChild(row);
     });
 }
@@ -21,6 +30,22 @@ function calcularPromedio() {
 function mostrarPromedio() {
     averageElement.innerHTML = `Seasons average: ${calcularPromedio()}`;
 }
+function showDetail(serie) {
+    detailElement.innerHTML = `
+    <div class="card">
+      <img class="card-img-top" src="${serie.image}" alt="${serie.name}">
+      <div class="card-body">
+        <h5 class="card-title">${serie.name}</h5>
+        <p class="card-text">${serie.description}</p>
+        <a href="${serie.webpage}" target="_blank">${serie.webpage}</a>
+      </div>
+    </div>
+  `;
+}
 renderSeries();
 mostrarPromedio();
+const firstSerie = series[0];
+if (firstSerie) {
+    showDetail(firstSerie);
+}
 //# sourceMappingURL=main.js.map
